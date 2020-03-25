c1396528d79fb1ae7ccacacf91da0816741dedb1
bogyshi <avanroi1@binghamton.edu>
2020-03-25 19:33:10 -0400

    added graphing capabilities

--- chat.py
+++ chat.py
@@ -3,7 +3,9 @@ import sys
 import pdb
 import numpy as np
 import re
-debug = True
+import matplotlib.pyplot as plt
+from spellchecker import SpellChecker
+debug = False
 class Chat:
     def _initUserClasses(self,isGM=False):
         """
@@ -13,54 +15,82 @@ class Chat:
         linkCounter=0
         imageCounter = 0
         emptyCounter = 0
+        self.maxTokenLength=20 # might want to shorten this still
+        self.maxMessageLength=20 # no need for messages this long
+        self.orderedMessages=[]
+        userMessageCount={}
+        self.personOrder=[] # this will let us tag each message with the person who sent it
         if(isGM):
-            toIter = self.rawJSON
+            toIter = [self.rawJSON]
             textKey = "text"
             userKey = "user_id"
         else:
-            toIter = self.rawJSON["messages"]
+            toIter = self.rawJSON
             textKey = "content"
             userKey= "sender_name"
         for u in self.userNames:
             self.users[u] = User(u)
-        for x in toIter:
-            counter = counter +1
-            if(counter < 10):
-                if(debug is True):
-                    print(x[textKey])
-                    print(x[userKey])
-                    #print(x["reaction"])
-            try:
-                
-                if('share' in x.keys()):
-                    linkCounter+=1
-                    print('found a link')
-                elif('photos' in x.keys()):
-                    imageCounter+=1
-                    print('found a photo')
-                elif('gifs' in x.keys()):
-                    print('found a gif')
-                elif('sticker' in x.keys()):
-                    print('found a sticker')
-                elif('video' in x.keys()):
-                    print('found a vid')
-                elif(textKey in x.keys()):
-                    if(x[textKey] is None):
-                        emptyCounter+=1
-                    else:
-                        if(isGM):
-                            pass
-                            #pdb.set_trace()
-                        self.users[x[userKey]].messages.append(x[textKey])
-                        toclean = x[textKey]
-                        temp=re.sub(r'[\'*‘*’*]','',toclean.strip().lower())
-                        temp=re.sub(r'[\,/.?!]',' ',temp)
-                        self.users[x[userKey]].cleanMessages.append(temp)
-                else:
-                    print("this is getting real fishy")
+            userMessageCount[u]=0
+        for y in toIter:
+            msToAppendInOrder=[]
+            userTags=[]
+            if(type(y) is dict):
+                toiter2 = y['messages']
+            else:
+                toiter2 = y
+            for x in toiter2:
+                counter = counter +1
+                if(counter < 10):
+                    if(debug is True):
+                        print(x[textKey])
+                        print(x[userKey])
+                        #print(x["reaction"])
+                try:
 
-            except KeyError:
-                pass
+                    if('share' in x.keys()):
+                        linkCounter+=1
+                        print('found a link')
+                    elif('photos' in x.keys()):
+                        imageCounter+=1
+                        print('found a photo')
+                    elif('gifs' in x.keys()):
+                        print('found a gif')
+                    elif('sticker' in x.keys()):
+                        print('found a sticker')
+                    elif('video' in x.keys()):
+                        print('found a vid')
+                    elif(textKey in x.keys()):
+                        if(x[textKey] is None):
+                            emptyCounter+=1
+                        else:
+                            if(isGM):
+                                pass
+                                #pdb.set_trace()
+                                #https://pypi.org/project/pyspellchecker/ #TODO USE AUTCORRECT TO FIX COMMON MISPELLINGS
+                            #
+                            toadd = x[textKey].lower().strip().rstrip()
+                            self.users[x[userKey]].messages.append(toadd)
+                            temp=re.sub(r'[\'*‘*’*]','',toadd.strip())# not exactly sure what this does tbh
+                            temp=re.sub(r"([?.!¿:;\\/])",r" \1 ",temp) # puts space around punctuation (gets its own word)
+                            #temp=re.sub(r'[\,/.?!:;]'," ",temp) # REPLACES punctuation with space instead (old version)
+
+                            temp = re.sub('[^a-z0-9?\.\\!:;/ ]+','',temp).strip().rstrip() # removes all non alpha numeric characters but leaves spaces
+                            self.users[x[userKey]].cleanMessages.append(temp)
+                            userTags.append(x[userKey])
+                            msToAppendInOrder.append(temp)
+                            userMessageCount[x[userKey]]+=1
+                    else:
+                        print("this is getting real fishy")
+
+                except KeyError:
+                    pass
+            self.orderedMessages.append(msToAppendInOrder[::-1])
+            self.personOrder.append(userTags[::-1])
+
+        self.orderedMessages = np.concatenate(np.array(self.orderedMessages),axis=None)
+        self.personOrder = np.concatenate(np.array(self.personOrder),axis=None)
+
+        #pdb.set_trace()
         print("num messages: " + str(counter))
         print("num links sent: " + str(linkCounter))
         print("num images sent: " + str(imageCounter))
@@ -77,6 +107,16 @@ class Chat:
         self.netDictionary = {}
         self.netMessages = 0
         self.netWords = 0
+        custXAxis=np.arange(0,len(self.userNames),1)
+        plt.bar(x=custXAxis,height=userMessageCount.values())
+        plt.title("number of messages per user")
+        hiddenNames = []
+        for x in self.userNames:
+            hiddenNames.append(x.split()[0])
+        plt.xticks(custXAxis,hiddenNames)
+        plt.xlabel("users")
+        plt.ylabel("total number of messages")
+        plt.savefig('./userMessageDistrib')
 
     def popSelfDict(self):
         for u in self.userNames:
@@ -109,6 +149,95 @@ class Chat:
     def dumpChatStats(self):
         for u in self.userNames:
             self.users[u].dumpStats()
+
+    def createCorpusAndTokens(self):
+        messages=[]
+        vocab = {}
+        lenDistrib = {}
+        messageDistrib = {}
+        #pdb.set_trace()
+        for m in self.orderedMessages:
+            newMessage=""
+            ms = m.split()
+            mlen =len(m.split())
+            if(mlen==0):
+                pass
+            elif(mlen>self.maxMessageLength):
+                ms = ms[0:self.maxMessageLength]
+            mlen=len(ms)
+            for v in ms:
+                #pdb.set_trace()
+                holdit = v
+                if(len(holdit)>=self.maxTokenLength):
+                    holdit = v[0:self.maxTokenLength] # truncate words, honestly might just get rid of them
+
+                if(holdit in vocab.keys()):
+                    vocab[holdit]+=1
+                else:
+                    vocab[holdit]=1
+                if(len(holdit) not in lenDistrib.keys()):
+                    lenDistrib[len(holdit)]=1
+                else:
+                    lenDistrib[len(holdit)]+=1
+                newMessage+=holdit+' '
+            if(mlen not in messageDistrib.keys()):
+                messageDistrib[mlen] = 1
+            else:
+                messageDistrib[mlen] += 1
+            messages.append(newMessage)
+        #print("Number unique mispelled words: "+str(len(setMisspelledWords)))
+        wordFrequency={}
+        breaks=[1000,500,200,100,50,10,5,3,1]
+        heights = np.zeros(len(breaks))
+        for x in vocab.keys():
+            val = vocab[x]
+            breakCounter = 0
+            while(val<breaks[breakCounter]):
+                breakCounter+=1
+            heights[breakCounter]+=1
+        custXAxis = np.arange(0,len(breaks),1)
+        plt.bar(x=custXAxis,height=heights)
+        plt.title("frequency of binned word frequencies")
+        plt.xticks(custXAxis, np.array(breaks).astype(str))
+        plt.xlabel("binned frequencies")
+        plt.ylabel("frequency")
+        plt.savefig('./wordFreqDistrib')
+        plt.clf()
+        plt.bar(x=lenDistrib.keys(),height=lenDistrib.values())
+        plt.title("frequency of word lengths")
+        plt.savefig('./wordLengthDistrib')
+        plt.clf()
+        plt.bar(x=messageDistrib.keys(),height=messageDistrib.values())
+        plt.title("frequency of message lengths")
+        plt.savefig('./messageLengthDistrib')
+        return np.array(messages).flatten(),vocab,self.personOrder
+
+
+    def getVocab(self):
+        vocab = {}
+        lenDistrib = {}
+        for u in self.users.keys():
+            for m in self.users[u].cleanMessages:
+                t = m.split()
+                for v in t:
+                    if(v in vocab.keys()):
+                        vocab[v]+=1
+                    else:
+                        vocab[v]=1
+                    if(len(v) not in lenDistrib.keys() and len(v)<20):
+                        lenDistrib[len(v)]=1
+                    elif(len(v)<20):
+                        lenDistrib[len(v)]+=1
+        print(lenDistrib)
+        plt.bar(x=lenDistrib.keys(),height=lenDistrib.values())
+        plt.savefig('./cmononwson')
+        return vocab
+
+    def getMessages(self):
+        messages=[]
+        for u in self.userNames:
+            messages.append(self.users[u].messages)
+        return np.array(messages).flatten()
 
     def __init__(self,names,data,isGM=False):
         print("in init")
--- parseData.py
+++ parseData.py
@@ -9,6 +9,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
+import pdb
+import pickle as pk
+#from gensim.test.utils import common_texts, get_tmpfile
+from gensim.models import Word2Vec
 
 #meant to prevent a stashed compiled file from preventing editing
 sys.dont_write_bytecode = True
@@ -41,9 +45,9 @@ def doHTBasic(thechat):
     targets=np.array([])
 
     for c in thechat.users:
-            
+
         holdsAllMessages = np.append(holdsAllMessages,thechat.users[c].cleanMessages)
-        
+
         targets=np.append(targets,np.repeat(c,len(thechat.users[c].cleanMessages)))
 
     text_clf = Pipeline([
@@ -99,13 +103,13 @@ def tryBagOfWords(thechat,trainsplit = 0.8,bestModel=None):
         mesSize = len(thechat.users[c].cleanMessages)
         trainInds = np.random.choice(mesSize, int(trainsplit*mesSize),replace=False).astype(int)
         testInds = np.setdiff1d(np.arange(mesSize),trainInds).astype(int)
-        
+
         #pdb.set_trace()
         print(testInds)
-            
+
         holdsAllTrainMessages = np.append(holdsAllTrainMessages,thechat.users[c].cleanMessages[trainInds])
         holdsAllTestMessages = np.append(holdsAllTestMessages,thechat.users[c].cleanMessages[testInds])
-        
+
         targetsTrain=np.append(targetsTrain,np.repeat(c,len(trainInds)))
         targetsTest=np.append(targetsTest,np.repeat(c,len(testInds)))
 
@@ -124,24 +128,26 @@ def tryBagOfWords(thechat,trainsplit = 0.8,bestModel=None):
     tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
     X_train_tf = tf_transformer.transform(X_train_counts)
     X_test_tf = tf_transformer.transform(X_test_counts)
-      
+
     if(doFreq):
-        xtrain = X_train_tf 
+        xtrain = X_train_tf
         xtest = X_test_tf
     else:
-        xtrain = X_train_counts 
+        xtrain = X_train_counts
         xtest=X_test_counts
-        
+
     clf = MultinomialNB(alpha=alpha).fit(xtrain, targetsTrain)
     return clf,(xtrain,targetsTrain),(xtest,targetsTest)
-    
+
 def getJson(pathName):
     if(debug is True):
         print(os.getcwd())
         print(os.listdir('/home/avanroi1/messengerData/testData/MuAlphaNuGammaOmicron_34ea4e8c3b'))
-
-    with open(pathName+'/message.json') as f:
-        data = json.load(f)
+    data = []
+    for f in np.sort(os.listdir(pathName))[::-1]: # have to order this in chronological order
+        with open(pathName+'/'+f) as f1:
+            data.append(json.load(f1))
+    #pdb.set_trace()
 
     return data
 
@@ -183,46 +189,65 @@ def startGroupMe(jsondata):
 
     return userids,maxName
 def main():
-    chowderPath ='/home/avanroi1/messages/inbox/chowder_g03zkk7sug'
-    weirdPathName = '/home/avanroi1/messages/inbox/mualphanugammaomicron_baafcd34pg'
-    groupmePathName = '/home/avanroi1/groupmeData/theboys'
-    groupmePathName = '/home/avanroi1/groupmeData/allupcs'
-
-
-    data = getJson(chowderPath)
-    data = getJson(weirdPathName)
-    gmdata = getJson(groupmePathName)
-    gcids,gcnames = startGroupMe(gmdata)
-    args = parseArgs()
-    numMessages = getNumMessages(data)
-    username = args.username
-    nameVec = data["participants"]
-    print(nameVec)
-    nameVec = [x['name'] for x in nameVec]
-    #nameVec.append(username)
-    if(debug is True):
+    datafs=[]
+    if(len(sys.argv)<2):
+        chowderPath ='/home/avanroi1/messages/inbox/chowder_g03zkk7sug'
+        weirdPathName = '/home/avanroi1/messages/inbox/mualphanugammaomicron_baafcd34pg'
+        groupmePathName = '/home/avanroi1/groupmeData/theboys'
+        #groupmePathName = '/home/avanroi1/groupmeData/allupcs'
+        datafs = [chowderPath,weirdPathName,groupmePathName]
+    else:
+        counter = 1
+        while(counter < len(sys.argv)):
+            datafs.append(sys.argv[counter])
+            counter+=1
+    for f in datafs:
+        data = getJson(f)
+        #gmdata = getJson(groupmePathName)
+        #gcids,gcnames = startGroupMe(gmdata)
+        args = parseArgs()
+        #numMessages = getNumMessages(data)
+        username = args.username
+        nameVec = data[0]["participants"]
         print(nameVec)
-    #c = Chat(nameVec,data)
-    c2= Chat(gcids,gmdata,isGM=True)
-    c2.popSelfDict()
-    c2.updateUserStats()
-    c2.updateNames(gcnames)
-    c2.printStats()
-    if(doModel):
-        bestmodel,data,targs = doHTBasic(c)
-        model,train,test = tryBagOfWords(c,bestModel=bestmodel)
+        nameVec = [x['name'] for x in nameVec]
+        #nameVec.append(username)
+        if(debug is True):
+            print(nameVec)
+        c2 = Chat(nameVec,data)
+        #c2= Chat(gcids,gmdata,isGM=True)
+        c2.popSelfDict()
+        c2.updateUserStats()
+        #c2.updateNames(gcnames)
+        c2.printStats()
+        corpus,vocab,userOrder = c2.createCorpusAndTokens()
+        pdb.set_trace()
+        with open('./conversation.pk','wb') as f:
+            pk.dump(corpus,f)
+        with open('./vocab.pk','wb') as f:
+            pk.dump(vocab,f)
+        with open('./userOrder.pk','wb') as f:
+            pk.dump(userOrder,f)
 
-        #pdb.set_trace()
-        whatevs = bestmodel.predict(["Miss you mangos"])
-        print(whatevs)
-        print(bestmodel.best_score_ )
-        print(doPipeLineAcc(bestmodel,data,targs))
-        print(bestmodel.best_params_)
-        trainAcc = testAcc(model,train)
-        trainAcc = testAcc(model,test)
-        multiClassAcc(model,train)
-        print("Test set results")
+        pdb.set_trace()
+        #print(corpus)
+        #model = Word2Vec(corpus, size=100, window=5, min_count=1, workers=4)
+        #model.save("word2vec.model")
+        if(doModel):
+            bestmodel,data,targs = doHTBasic(c2)
+            model,train,test = tryBagOfWords(c2,bestModel=bestmodel)
 
-        multiClassAcc(model,test)
+            #pdb.set_trace()
+            whatevs = bestmodel.predict(["Miss you mangos"])
+            print(whatevs)
+            print(bestmodel.best_score_ )
+            print(doPipeLineAcc(bestmodel,data,targs))
+            print(bestmodel.best_params_)
+            trainAcc = testAcc(model,train)
+            trainAcc = testAcc(model,test)
+            multiClassAcc(model,train)
+            print("Test set results")
+
+            multiClassAcc(model,test)
     #c.dumpChatStats()
 main()
